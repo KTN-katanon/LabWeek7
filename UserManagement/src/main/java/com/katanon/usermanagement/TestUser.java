09830be6d45431caeb9309830a1d018c9a83dfab
KTN-katanon <66160196@go.buu.ac.th>
2024-08-15 18:29:40 +0700

    Update TestUser.java
    Test print User

--- UserManagement/src/main/java/com/katanon/usermanagement/TestUser.java
+++ UserManagement/src/main/java/com/katanon/usermanagement/TestUser.java
@@ -4,10 +4,27 @@
  */
 package com.katanon.usermanagement;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nonku
  */
 public class TestUser {
-    
+    public static void main(String[] args) {
+        User admin = new User(1,"admin","Administrator","pass@1234",'M','A');
+        User user1 = new User(2,"user1","User 1","pass@1234",'F','U');
+        User user2 = new User(3,"user2","User 2","pass@1234",'M','U');
+        ArrayList<User> userList = new ArrayList<User>(); // Generic type
+        userList.add(admin);
+        userList.add(user1);
+        userList.add(user2);
+        for(int i =0; i < userList.size();i++){
+            System.out.println(userList.get(i));
+        }
+        
+        for(User u :userList){
+            System.out.println(u);
+        }
+    }
 }
