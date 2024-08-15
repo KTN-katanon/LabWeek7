d2f788a5267c71f4483f089b2e654cf6ea69fd1f
KTN-katanon <66160196@go.buu.ac.th>
2024-08-15 19:39:04 +0700

    Update UserService.java
    Fix getUserById

--- UserManagement/src/main/java/com/katanon/usermanagement/UserService.java
+++ UserManagement/src/main/java/com/katanon/usermanagement/UserService.java
@@ -11,10 +11,11 @@ import java.util.ArrayList;
  * @author nonku
  */
 public class UserService {
+
     private static ArrayList<User> userList = new ArrayList<>();
     private static int lastId = 1;
-    
-    public static User addUser(User newUser){
+
+    public static User addUser(User newUser) {
         newUser.setId(lastId++);
         userList.add(newUser);
         return newUser;
@@ -23,32 +24,37 @@ public class UserService {
     public static ArrayList<User> getUsers() {
         return userList;
     }
-    
-    public static User getUser(int index){
+
+    public static User getUser(int index) {
         return userList.get(index);
     }
-    
-    public static User getUserById(int id){
-        return userList.get(id);
+
+    public static User getUserById(int id) {
+        for(User u: userList){
+            if(u.getId() == id){
+                return u;
+            } 
+        }
+        return null;
     }
-    
+
     public static int getSize() {
         return userList.size();
     }
-    
-    public static User updateUser(int index, User user){
+
+    public static User updateUser(int index, User user) {
         userList.set(index, user);
         return user;
     }
-    
-    public static User deleteUser(int index){
+
+    public static User deleteUser(int index) {
         return userList.remove(index);
-        
+
     }
-    
+
     public static void printList() {
         ArrayList<User> list = UserService.getUsers();
-        for(User u :list){
+        for (User u : list) {
             System.out.println(u);
         }
     }
