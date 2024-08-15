50a469a72c0d85f55b75f373e94b94d90e2995ee
KTN-katanon <66160196@go.buu.ac.th>
2024-08-15 18:14:47 +0700

    Create project

---
+++ UserManagement/src/main/java/com/katanon/usermanagement/UserManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katanon.usermanagement;
+
+/**
+ *
+ * @author nonku
+ */
+public class UserManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
